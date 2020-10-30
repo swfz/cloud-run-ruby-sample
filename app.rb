024e67358758a2cb2cac4e3fd8d9c438a3b8e4b7
swfz <sawafuji.09@gmail.com>
2020-10-31 05:16:03 +0900

    wip: local development

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'json'
 require 'base64'
 require 'google/cloud/storage'
+require "google/cloud/secret_manager"
 
 set :bind, "0.0.0.0"
 port = ENV["PORT"] || "8080"
@@ -15,6 +16,15 @@ post "/" do
 
   storage = Google::Cloud::Storage.new
   bucket = storage.bucket ENV["BUCKET"]
+  project_id = ENV["PROJECT_ID"]
+
+  client = Google::Cloud::SecretManager.secret_manager_service
+  key = client.secret_version_path project: project_id, secret: 'my-secret', secret_version: 'latest'
+  res = client.access_secret_version name: key
+  p 'secret - = = = = = ='
+  p key
+  p res.payload
+  p res.payload.data
 
   local_file_path = "./Gemfile"
   storage_file_path = "Gemfile"
