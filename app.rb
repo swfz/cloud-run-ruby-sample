3eb3b89932483b94ebc6d7a403d0f993f4f2a357
swfz <sawafuji.09@gmail.com>
2020-11-08 00:08:28 +0900

    feat: divide path

--- app.rb
+++ app.rb
@@ -16,18 +16,17 @@ post "/" do
   p params
   # p Base64.decode64(params['message']['data'])
 
+  name = ENV["NAME"] || "World"
+  "Hello #{name}!"
+  status 200
+end
+
+post "/storage" do
+  p 'called storage'
+  p 'params ------'
+  p params
   storage = Google::Cloud::Storage.new
   bucket = storage.bucket ENV["BUCKET"]
-  project_id = ENV["PROJECT_ID"]
-
-  client = Google::Cloud::SecretManager.secret_manager_service
-  key = client.secret_version_path project: project_id, secret: 'sample-secret', secret_version: 'latest'
-  p 'key - = = = = = ='
-  p key
-  res = client.access_secret_version name: key
-  p 'secret - = = = = = ='
-  p res.payload
-  p res.payload.data
 
   local_file_path = "./Gemfile"
   storage_file_path = "Gemfile"
@@ -35,7 +34,24 @@ post "/" do
   file = bucket.create_file local_file_path, storage_file_path
   p file
 
-  name = ENV["NAME"] || "World"
-  "Hello #{name}!"
+  status 200
+end
+
+post "/secret_manager" do
+  p 'called secret_manager'
+  p 'params ------'
+  p params
+
+  project_id = ENV["PROJECT_ID"]
+
+  client = Google::Cloud::SecretManager.secret_manager_service
+  key = client.secret_version_path project: project_id, secret: 'sample-secret', secret_version: 'latest'
+  p 'key ------'
+  p key
+  res = client.access_secret_version name: key
+  p 'secret ------'
+  p res.payload
+  p res.payload.data
+
   status 200
 end
